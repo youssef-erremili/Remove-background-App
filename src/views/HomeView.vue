5d584ab319710fb85f2120288abd90cd97bbc4f8
youssef-erremili <yousseferremili19@gmail.com>
2025-04-06 18:05:53 +0100

    WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
     <AppNavbar />
     <HeroSection />
+    <FeatureCard />
 </template>
 
 <script setup>
 import AppNavbar from '@/components/common/AppNavbar.vue';
 import HeroSection from '@/components/home/HeroSection.vue';
+import FeatureCard from '@/components/home/FeatureCard.vue';
 </script>
