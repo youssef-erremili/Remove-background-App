cd37271022a4f9215bbe287ba6472f2d924be520
youssef-erremili <yousseferremili19@gmail.com>
2025-04-06 18:04:46 +0100

    WIP

---
+++ src/components/common/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-full py-16 mt-10">
+        <div class="max-w-6xl mx-auto px-4 text-center">
+            <h1 class="text-4xl md:text-5xl font-bold text-gray-800 mb-6">
+                {{ title }}
+            </h1>
+            <p class="text-xl text-gray-700 max-w-3xl mx-auto">
+                {{ slogane }}
+            </p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    title: {
+        Type: String,
+        required: true,
+    },
+    slogane: {
+        Type: String,
+        required: true,
+    },
+})
+</script>
