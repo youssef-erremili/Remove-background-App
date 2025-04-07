f700f19e58e5a92393c1eda5a06c4f6daefb7a28
youssef-erremili <yousseferremili19@gmail.com>
2025-04-07 22:53:12 +0100

    WIP

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,14 @@
     <AppNavbar />
     <HeroSection />
     <FeatureCard />
+    <AppTestimonial />
+    <AppFooter />
 </template>
 
 <script setup>
 import AppNavbar from '@/components/common/AppNavbar.vue';
 import HeroSection from '@/components/home/HeroSection.vue';
 import FeatureCard from '@/components/home/FeatureCard.vue';
+import AppFooter from '@/components/common/AppFooter.vue';
+import AppTestimonial from '@/components/common/AppTestimonial.vue';
 </script>
