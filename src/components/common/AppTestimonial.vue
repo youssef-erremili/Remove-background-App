0896c17de9341dfdf05eb8682887deaf0e72280c
youssef-erremili <yousseferremili19@gmail.com>
2025-04-07 22:54:44 +0100

    WIP

---
+++ src/components/common/AppTestimonial.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="bg-white py-16 px-4 my-16">
+    <div class="max-w-7xl mx-auto">
+        <div class="flex flex-wrap justify-between">
+            <!-- Testimonial 1 -->
+            <div class="w-full md:w-[30%] bg-white rounded-2xl p-8 shadow-sm mb-6 md:mb-0 flex flex-col min-h-[400px]">
+                <div class="flex-grow">
+                    <p class="text-gray-500 text-2xl">"</p>
+                    <p class="text-gray-600 mb-4">
+                        I've been using Removal.AI for a while, I guess this one is a pretty good match. I have
+                        downloaded the preview image at 1000px resolution for free. The basic editor is also quite
+                        interesting. Worth giving a try!
+                    </p>
+                </div>
+                <div class="flex items-center mt-auto pt-4">
+                    <div>
+                        <p class="font-medium text-gray-800">
+                            <span class="text-blue-500 font-medium">@ </span>Powell Gao
+                        </p>
+                        <p class="text-gray-500 text-[12.5px]">Software Developer - imagecolorizer.com</p>
+                    </div>
+                    <div class="ml-auto">
+                        <div class="w-12 h-12 rounded-full border-2 border-gray-300 overflow-hidden">
+                            <img src="../../../public/images/Testimonials1.png" alt="Powell Gao" class="w-full h-full object-cover" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Testimonial 2 -->
+            <div class="w-full md:w-[30%] bg-white rounded-2xl p-8 shadow-sm mb-6 md:mb-0 flex flex-col min-h-[400px]">
+                <div class="flex-grow">
+                    <p class="text-gray-500 text-2xl">"</p>
+                    <p class="text-gray-600 mb-4">
+                        Well I'm particularly surprised at how accurate and smooth are the cut-out edges. I always
+                        wanted a super fast background remover. Finally I do not have to open up Photoshop to get
+                        the job done.
+                    </p>
+                </div>
+                <div class="flex items-center mt-auto pt-4">
+                    <div>
+                        <p class="font-medium text-gray-800">
+                            <span class="text-blue-500 font-medium">@ </span>Andrea Mangano
+                        </p>
+                        <p class="text-gray-500 text-[12.5px]">Product Designer - assemble.tv</p>
+                    </div>
+                    <div class="ml-auto">
+                        <div class="w-12 h-12 rounded-full border-2 border-gray-300 overflow-hidden">
+                            <img src="../../../public/images/Testimonials2.png" alt="Andrea Mangano" class="w-full h-full object-cover" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Testimonial 3 -->
+            <div class="w-full md:w-[30%] bg-white rounded-2xl p-8 shadow-sm mb-6 md:mb-0 flex flex-col min-h-[400px]">
+                <div class="flex-grow">
+                    <p class="text-gray-500 text-2xl">"</p>
+                    <p class="text-gray-600 mb-4">
+                        I have been using Photoroom for almost 2 years now, mainly for Instagram. Photoroom is so user-friendly and makes my work complete. The Photoroom group on Facebook is super, 
+                    </p>
+                </div>
+                <div class="flex items-center mt-auto pt-4">
+                    <div>
+                        <p class="font-medium text-gray-800">
+                            <span class="text-blue-500 font-medium">@ </span>Youssef Erremili
+                        </p>
+                        <p class="text-gray-500 text-[12.5px]">Tech Savvy - quicksync.me</p>
+                    </div>
+                    <div class="ml-auto">
+                        <div class="w-12 h-12 rounded-full border-2 border-gray-300 overflow-hidden">
+                            <img src="../../../public/images/Testimonials3.png" alt="Youssef Erremili" class="w-full h-full object-cover" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup></script>
