abe4064579d68941618aab63e46a5085d5827119
youssef-erremili <yousseferremili19@gmail.com>
2025-04-13 21:59:25 +0100

    AppUpload

--- src/components/common/AppUpload.vue
+++ src/components/common/AppUpload.vue
@@ -1,13 +1,49 @@
+<script setup>
+import router from '@/router';
+import { ref, inject } from 'vue';
+
+// inital value if file input
+const triggerAlert = inject('triggerAlert')
+const uploadedImage = inject('uploadedImage')
+
+const handleInputMeme = (event) => {
+    const file = event.target.files[0];
+    const allowedMimeTypes = ["image/jpeg", "image/png", "image/gif"];
+    
+    if (!file) {
+        triggerAlert("Make sure you upload right file extension!");
+        return;
+    }
+    
+    if (!allowedMimeTypes.includes(file.type)) {
+        triggerAlert("Unsupported file type!")
+        return
+    }
+    
+    const reader = new FileReader()
+    reader.onload = function () {
+        const memeFile = reader.result
+        uploadedImage.value = memeFile;
+        triggerAlert("File uploaded successfully!")
+        router.push({ path: '/upload' })
+    }
+    
+    reader.readAsDataURL(file)
+}
+
+</script>
+
 <template>
     <div>
-        <div class="w-full max-w-md py-12 rounded-3xl shadow-[0_12px_48px_-8px_rgba(0,0,0,0.16)]"> 
+        <div class="w-full max-w-md py-12 rounded-3xl shadow-[0_12px_48px_-8px_rgba(0,0,0,0.16)]">
             <div class="flex flex-col items-center justify-center space-y-6">
                 <div class="relative p-3 overflow-hidden">
                     <button class="max-w-xs cursor-pointer flex items-center justify-center gap-2 py-4 px-10 bg-[#4e7efe] hover:bg-[#376cff] text-white font-medium rounded-2xl shadow-md transition-all duration-200 transform hover:scale-[1.02]">
                         <img src="../../../public/images/upload.png" alt="icon upload">
                         <span class="font-light">Upload Your Image</span>
                     </button>
-                    <input class="cursor-pointer w-full h-full opacity-0 absolute top-0 left-0" type="file" name="file" id="file">
+                    <input @change="handleInputMeme" class="cursor-pointer w-full h-full opacity-0 absolute top-0 left-0" type="file" name="file"
+                        accept="image/jpeg, image/png, image/gif, image/webp" id="file">
                 </div>
                 <div class="flex flex-col items-center text-center space-y-2">
                     <p class="text-gray-500">Or Drop an image,</p>
@@ -22,5 +58,3 @@
         </div>
     </div>
 </template>
-
-<script setup></script>
